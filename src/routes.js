d169544e5e5923229406cd08146ce639a7c4e0a1
JonathanAmarall <jonathan@speedrs.com.br>
2020-04-06 11:07:02 -0300

    Created pagination and list appointments

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,8 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -14,5 +16,9 @@ routes.use(authMiddleware);
     routes.post('/users', UserController.store);
     routes.put('/users', UserController.update);
     routes.post('/files', upload.single('file') ,FileController.store);
+    routes.get('/providers', ProviderController.index);
+
+    routes.get('/appointments', AppointmentController.index);
+    routes.post('/appointments', AppointmentController.store);
 
 export default routes;
