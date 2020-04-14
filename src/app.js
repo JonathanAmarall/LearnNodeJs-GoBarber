0a447992f8e51e2f21490d026a62665c7cb9ed95
JonathanAmarall <jonathan@speedrs.com.br>
2020-04-14 14:24:14 -0300

    Include sentry for to monitor errors in application

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
 import express from 'express';
 import path from 'path';
+import Youth from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
 import routes from './routes';
+import sentryConfig from './config/sentry';
 import './database';
 
 class App {
     constructor() {
         this.server = express();
+
+        Sentry.init(sentryConfig);
+
         this.middlewares();
         this.routes();
+        this.exceptionHandler();
     }
 
     middlewares() {
+        this.server.use(Sentry.Handlers.requestHandler());
         this.server.use(express.json());
         this.server.use('/files',
             express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
@@ -19,6 +28,15 @@ class App {
 
     routes() {
         this.server.use(routes);
+        this.server.use(Sentry.Handlers.errorHandler());
+    }
+
+    exceptionHandler(){
+        this.server.use(async (err, req, res, next) => {
+            const errors = await new Youth(err, req).toJSON();
+
+            return res.status(500).json(errors);
+        });
     }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+    dsn: 'https://2663d46da5bc4f1caf659b79e884a461@o377769.ingest.sentry.io/5200344'
+};
