4e91968961bf7b49592bbfb52e8aacf219bb2199
JonathanAmarall <jonathan@speedrs.com.br>
2020-04-14 09:50:05 -0300

    Carrying out email with queues, with redis

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,11 +1,12 @@
 import * as Yup from 'yup';
 import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt'
-import Mail from '../../lib/Mail';
 import Appointment from '../models/Appointment';
 import User from "../models/User";
 import File from '../models/File';
 import Notification from '../schemas/Notification';
+import Queue from '../../lib/Queue';
+import CancellationMail from '../jobs/CancellationMail';
 
 class AppointmentController{
     async index(req, res){
@@ -145,22 +146,9 @@ class AppointmentController{
 
         (await appointment).save();
 
-        await Mail
-        .sendMail({
-            to: `${appointment.provider.name} <${appointment.provider.email}`,
-            subject: 'Agendamento cancelado',
-            template: 'cancellation',
-            context: {
-                provider: appointment.provider.name,
-                user: appointment.user.name,
-                date: format(
-                    appointment.date,
-                     "'dia' dd 'de' MMMM', às' H:mm'h'",
-                     {
-                         locale: pt
-                     })
-            }
-        });
+        await Queue.add(CancellationMail.key, {
+            appointment,
+        })
 
         return res.json(appointment);
     }
