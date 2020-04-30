707d62401b00292fff5ea99f19943a78f0ef8ff9
JonathanAmarall <jonathan@speedrs.com.br>
2020-04-30 20:42:31 -0300

    Add env.example file

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,7 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-    'prettier'
-  ],
+  extends: ['airbnb-base', 'prettier'],
   plugins: ['prettier'],
   globals: {
     Atomics: 'readonly',
@@ -17,6 +14,7 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+      "prettier/prettier": "error",
     "class-methods-use-this" : "off",
     "no-param-reassign": "off",
     "camelcase" : "off",
